9cb3cd996bac4d722a39d7c5409d6ef2795a8666
romanthium <romanthium@gmail.com>
2022-10-17 23:33:13 +0200

    renamed class to SubstringSearch

--- substring_search/SubstringSearch.java
+++ substring_search/SubstringSearch.java
@@ -2,7 +2,7 @@ package com.telran.berlin.algorithms.substring_search;
 
 import java.util.Arrays;
 
-public class PrefixFunction {
+public class SubstringSearch {
     public static void main(String[] args) {
 //        String s = "avada kedavra";
 //        System.out.println(Arrays.toString(prefixFunction(s)));   // test for prefixFunction
@@ -11,7 +11,7 @@ public class PrefixFunction {
         int startIdx = 0;
         while (true) {
             startIdx = substringSearch(text, substring, startIdx);  // 6 - index (7 - position)
-            if(startIdx == -1) break;
+            if (startIdx == -1) break;
             System.out.println(startIdx++);
         }
     }
